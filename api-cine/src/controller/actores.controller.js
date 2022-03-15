f9d22f2ef714efb03078b81a0b82fbb8f96cafa0
EbiMania <n_agustin@hotmail.com>
2022-03-15 15:52:38 -0300

    agregadas rutas basicas y controladores
    en la ruta de peliculas hice un get tambien para obtener todas las peliculas de la base de datos

---
+++ api-cine/src/controller/actores.controller.js
@@ -0,0 +1,8 @@
+const { Actores } = require("../db/models/actores");
+
+//desarrollar aqui las funciones para los pedidos a la base de datos
+
+
+module.exports = {
+    //funciones a exportar para las rutas
+}

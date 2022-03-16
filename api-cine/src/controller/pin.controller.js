f060aba8ff1cf4993498fc29ee1603c6321431d6
Agustín Salto <88252174+EbiMania@users.noreply.github.com>
2022-03-16 12:14:11 -0300

    Update pin.controller.js

--- api-cine/src/controller/pin.controller.js
+++ api-cine/src/controller/pin.controller.js
@@ -1,7 +1,7 @@
 const { Actores } = require("../db/models/actores");
 
 //desarrollar aqui las funciones para los pedidos a la base de datos
-const pin = async (req, res) => {
+const pin = (req, res) => {
   return res.json({ msg: "pong" });
 };
 

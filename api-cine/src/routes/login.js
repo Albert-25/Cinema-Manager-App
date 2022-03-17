21cecbbcfe43b4c626c8d835bbcb62e92d194c29
JSilva723 <silvajonatanivan@gmail.com>
2022-03-17 07:33:08 -0300

    Added login and singup routes

--- api-cine/src/routes/login.js
+++ api-cine/src/routes/login.js
@@ -1,22 +1,6 @@
-const { User } = require('../db/models/user')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const { PRIVATEKEY } = require('../../config')
+const LogIn = require("express").Router();
+const { logIn } = require('../controller/login.controller');
 
-const logIn = async (req, res, next) => {
-  if (!req.body.username) return res.status(406).json({ msg: 'The username is required.'})
-  if (!req.body.passwd) return res.status(406).json({ msg: 'The password is required.'})
-  try {
-    let [user] = await User.findAll()
-    if (user) {
-      if (user.username !== req.body.username) return res.status(406).json({ msg: 'The username is incorrect.'})
-      const isMatch = await bcrypt.compare(req.body.passwd, user.passwd)
-      if (!isMatch) return res.status(406).json({ msg: 'The password is incorrect.'})
-    } else {
-      user = await User.create({ username: req.body.username, passwd: req.body.passwd })
-    }
-    return res.send({ token: jwt.sign({ id: user.id, username: user.username }, PRIVATEKEY) })
-  } catch (err) { next(err) }
-}
+LogIn.post('/', logIn);
 
-module.exports = { logIn }
+module.exports = { LogIn };

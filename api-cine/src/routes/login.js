34834e535b5ff3dd09f9fd3ec4870e4fa9929328
JSilva723 <silvajonatanivan@gmail.com>
2022-03-18 07:49:34 -0300

    Fix name of router in login

--- api-cine/src/routes/login.js
+++ api-cine/src/routes/login.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { logIn } = require('../controller/login.controller');
 
-LogrouterIn.post('/', logIn);
+router.post('/', logIn);
 
 module.exports = router;

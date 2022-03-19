064da422b6200ea97b33c405b1ea88533acb80af
JSilva723 <silvajonatanivan@gmail.com>
2022-03-19 15:25:50 -0300

    Refactor start

--- api-cine/src/db/connection.js
+++ api-cine/src/db/connection.js
@@ -1,23 +1,19 @@
 const { Sequelize } = require('sequelize')
 const { DATABASE_URI } = require('../../config.js')
-const sequelize = new Sequelize(DATABASE_URI, {
-  logging: false,
-  native: false,
-  dialectOptions: {
-     ssl: {
-        require: true,
-        rejectUnauthorized: false,
-     },
-  },
-})
 
-sequelize
-   .authenticate()
-   .then(() => {
-      console.log("Connection has been established successfully.");
-   })
-   .catch((err) => {
-      console.error("Unable to connect to the database:", err);
-   });
+const opt = process.env.NODE_ENV === 'production'
+   ? {
+      ssl: {
+         require: true,
+         rejectUnauthorized: false
+      }
+   }
+   : null;
+
+const sequelize = new Sequelize(DATABASE_URI, {
+   logging: false,
+   native: false,
+   dialectOptions: opt
+})
 
 module.exports = { sequelize }

ddc16395e371bd522b4bc95a68210d326427a1de
JSilva723 <silvajonatanivan@gmail.com>
2022-03-21 19:14:49 -0300

    Change passwd to password in user model

--- api-cine/src/controller/login.controller.js
+++ api-cine/src/controller/login.controller.js
@@ -4,11 +4,13 @@ const jwt = require('jsonwebtoken')
 const { PRIVATEKEY } = require('../../config')
 
 const logIn = async (req, res, next) => {
-  if (!req.body.email) return res.status(406).json({ msg: 'The email is required.' })
-  if (!req.body.passwd) return res.status(406).json({ msg: 'The password is required.' })
+  const { email, password } = req.body
+  if (!email) return res.status(406).json({ msg: 'The email is required.' })
+  if (!password) return res.status(406).json({ msg: 'The password is required.' })
   try {
-    const user = await User.findOne({ where: { email: req.body.email, role: req.body.role }})
-    const isMatch = await bcrypt.compare(req.body.passwd, user.passwd)
+    const user = await User.findOne({ where: { email: email }})
+    if (!user) return res.status(406).json({ msg: 'The email is incorrect.' })
+    const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) return res.status(406).json({ msg: 'The password is incorrect.' })
     return res.send({ token: jwt.sign({ id: user.id, role: user.role }, PRIVATEKEY) })
   } catch (err) { next(err) }
--- api-cine/src/controller/singup.controller.js
+++ api-cine/src/controller/singup.controller.js
@@ -3,11 +3,13 @@ const jwt = require('jsonwebtoken')
 const { PRIVATEKEY } = require('../../config')
 
 const singUp = async (req, res, next) => {
-  if (!req.body.email) return res.status(406).json({ msg: 'The email is required.' })
-  if (!req.body.passwd) return res.status(406).json({ msg: 'The password is required.' })
+  const { email, password } = req.body
+  if (!email) return res.status(406).json({ msg: 'The email is required.' })
+  if (!password) return res.status(406).json({ msg: 'The password is required.' })
   const role = req.body.role || 'portal'
   try {
-    const user = await User.create({ email: req.body.email, passwd: req.body.passwd, role: role })
+    const user = await User.create({ email: email, password: password, role: role })
+    console.log(user)
     return res.send({ token: jwt.sign({ id: user.id, role: user.role }, PRIVATEKEY) })
   } catch (err) { next(err) }
 }

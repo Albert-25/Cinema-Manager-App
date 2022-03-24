d9b37a80205c58223d24bcef93d3c83072075498
VicenMC <vicencodinglab@gmail.com>
2022-03-24 10:56:26 -0600

    Carousel fix

--- client-cine/src/components/Carousel/Carousel.js
+++ client-cine/src/components/Carousel/Carousel.js
@@ -1,47 +1,41 @@
-
-import Slider from "infinite-react-carousel"
+import Slider from "infinite-react-carousel";
 import { useSelector } from "react-redux";
 
 // import "./Carousel.css"
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import React, { useEffect } from "react";
 
-
-let arrais = [{
-    id: 1,
-    titulo: "Movie Not found",
-    sipnosis: "???",
-    poster: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/4QNRHYDIZFEJTINRQFU5BSVHSA.jpg",
-    duracion: "???",
-    pais: "???",
-    clasificacion: "???",
-    director: "???",
-    puntuación: "???",
-    distribuidora: "???",
-    genero: ["???"]
-},
-{
-    id: 2,
-    titulo: "Movie Not found2",
-    sipnosis: "???",
-    poster: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/4QNRHYDIZFEJTINRQFU5BSVHSA.jpg",
-    duracion: "???",
-    pais: "???",
-    clasificacion: "???",
-    director: "???",
-    puntuación: "???",
-    distribuidora: "???",
-    genero: ["???"]
-}]
-
+let arrais = [
+    {
+        id: 1,
+        titulo: "Movie Not found",
+        sipnosis: "???",
+        poster: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/4QNRHYDIZFEJTINRQFU5BSVHSA.jpg",
+        duracion: "???",
+        pais: "???",
+        clasificacion: "???",
+        director: "???",
+        puntuación: "???",
+        distribuidora: "???",
+        genero: ["???"],
+    },
+    {
+        id: 2,
+        titulo: "Movie Not found2",
+        sipnosis: "???",
+        poster: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/4QNRHYDIZFEJTINRQFU5BSVHSA.jpg",
+        duracion: "???",
+        pais: "???",
+        clasificacion: "???",
+        director: "???",
+        puntuación: "???",
+        distribuidora: "???",
+        genero: ["???"],
+    },
+];
 
 const Carousel = (AllMovies) => {
-
-
-
-
-
     // const AllPelis = useSelector((state) => state.PelisAll)
     // let arregloFinal = []
 
@@ -53,32 +47,28 @@ const Carousel = (AllMovies) => {
     //             a.puntuación < b.puntuación ? 1 : b.puntuación < a.puntuación ? -1 : 0
     //         )
 
-
     //         arregloFinal = arreglar.slice(0, 3)
     //         console.log("arreglo final", arregloFinal)
     //     }
     // }, [AllPelis]);
 
-
-
-    return (<section className='slider'>
-        <h1 className='slider__title'>
-            Estrenos imperdibles!
-        </h1>
-        <Slider className="slider__content" >
-            {
-                arrais.map(elm => {
-                    return (<div className='slider__content--item' key={elm.id}>
-                        <img src={elm.poster} alt={elm.titulo}></img>
-                        {/* <Link to={`MovieDetails/${elm.id}`}>
+    return (
+        <section className="slider">
+            <h1 className="slider__title">Estrenos imperdibles!</h1>
+            <Slider className="slider__content">
+                {arrais.map((elm) => {
+                    return (
+                        <div className="slider__content--item" key={elm.id}>
+                            <img src={elm.poster} alt={elm.titulo}></img>
+                            <Link to={`MovieDetails/${elm.id}`}>
                             <p className='slider-description'>{elm.titulo}</p>
-                        </Link> */}
+                        </Link> 
+                        </div>
+                    );
+                })}
+            </Slider>
+        </section>
+    );
+};
 
-                    </div>)
-                })
-            }
-        </Slider>
-    </section>)
-}
-
-export default Carousel
+export default Carousel;

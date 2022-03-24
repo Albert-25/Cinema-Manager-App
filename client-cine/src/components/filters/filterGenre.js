3c1c71c17a5f808386b05cf0428a52f35e047a65
Elian <eliancarlogm@gmail.com>
2022-03-23 23:59:19 -0430

    chores: prove filters

--- client-cine/src/components/filters/filterGenre.js
+++ client-cine/src/components/filters/filterGenre.js
@@ -3,151 +3,157 @@ import { useSelector } from "react-redux";
 import "./filterGenre.css";
 
 export default function FiltroGeneros({
-  FiltradoGeneros,
-  FiltradoCast,
-  FiltradoGenreAndCast,
+   FiltradoGeneros,
+   FiltradoCast,
+   FiltradoGenreAndCast,
 }) {
-  const Genres = useSelector((state) => state.GenresAll);
-  const Cast = useSelector((state) => state.CastAll);
-  let GenresName = Genres.map((e) => e.genero)
-  let CastName = Cast.map((e) => e.nombre)
+   const Genres = useSelector((state) => state.GenresAll);
+   const Cast = useSelector((state) => state.CastAll);
+   let GenresName = Genres.map((e) => e.genero);
+   let CastName = Cast.map((e) => e.nombre);
 
-  const [selectedGenres, setSelectedGenres] = React.useState([]);
-  const [selectedCast, setSelectedCast] = React.useState([]);
+   const [selectedGenres, setSelectedGenres] = React.useState([]);
+   const [selectedCast, setSelectedCast] = React.useState([]);
 
-  const handleOnSubmit = (evt) => {
-    evt.preventDefault();
-    if (selectedGenres.length !== 0 && selectedCast.length === 0) {
-      setTimeout(() => {
-        FiltradoGeneros(selectedGenres);
-      }, 1000);
-    } else if (selectedGenres.length === 0 && selectedCast.length !== 0) {
-      setTimeout(() => {
-        FiltradoCast(selectedCast);
-      }, 1000);
-    } else if (selectedGenres.length !== 0 && selectedCast.length !== 0) {
-      let completeArray = [];
-      completeArray.push(selectedGenres);
-      completeArray.push(selectedCast);
-      setTimeout(() => {
-        FiltradoGenreAndCast(completeArray);
-      }, 1000);
-    } else {
-      FiltradoGeneros();
-    }
-  };
+   const handleOnSubmit = (evt) => {
+      evt.preventDefault();
+      if (selectedGenres.length !== 0 && selectedCast.length === 0) {
+         setTimeout(() => {
+            FiltradoGeneros(selectedGenres);
+         }, 1000);
+      } else if (selectedGenres.length === 0 && selectedCast.length !== 0) {
+         setTimeout(() => {
+            FiltradoCast(selectedCast);
+         }, 1000);
+      } else if (selectedGenres.length !== 0 && selectedCast.length !== 0) {
+         let completeArray = [];
+         completeArray.push(selectedGenres);
+         completeArray.push(selectedCast);
+         setTimeout(() => {
+            FiltradoGenreAndCast(completeArray);
+         }, 1000);
+      } else {
+         FiltradoGeneros();
+      }
+   };
 
-  const changeCast = (evt) => {
-    setSelectedCast((newCast) => [...newCast, evt.target.value]);
-    document
-      .getElementById(evt.target.value)
-      .setAttribute("disabled", "disabled");
-  };
+   const changeCast = (evt) => {
+      setSelectedCast((newCast) => [...newCast, evt.target.value]);
+      document
+         .getElementById(evt.target.value)
+         .setAttribute("disabled", "disabled");
+   };
 
-  const changeGenres = (evt) => {
-    setSelectedGenres((newGenre) => [...newGenre, evt.target.value]);
-    document
-      .getElementById(evt.target.value)
-      .setAttribute("disabled", "disabled");
-  };
+   const changeGenres = (evt) => {
+      setSelectedGenres((newGenre) => [...newGenre, evt.target.value]);
+      document
+         .getElementById(evt.target.value)
+         .setAttribute("disabled", "disabled");
+   };
 
-  const handleOnClickGenres = (item) => {
-    let index = selectedGenres.indexOf(item);
-    let newArr = selectedGenres.filter(
-      (e) => selectedGenres.indexOf(e) !== index
-    );
-    setSelectedGenres(newArr);
-    document.getElementById(item).removeAttribute("disabled");
-    document.getElementById("defaultGenres").selectedIndex = 0;
-  };
+   const handleOnClickGenres = (item) => {
+      let index = selectedGenres.indexOf(item);
+      let newArr = selectedGenres.filter(
+         (e) => selectedGenres.indexOf(e) !== index
+      );
+      setSelectedGenres(newArr);
+      document.getElementById(item).removeAttribute("disabled");
+      document.getElementById("defaultGenres").selectedIndex = 0;
+   };
 
-  const handleOnClickCast = (item) => {
-    let index = selectedCast.indexOf(item);
-    let newArr = selectedCast.filter((e) => selectedCast.indexOf(e) !== index);
-    setSelectedCast(newArr);
-    document.getElementById(item).removeAttribute("disabled");
-    document.getElementById("defaultCast").selectedIndex = 0;
-  };
+   const handleOnClickCast = (item) => {
+      let index = selectedCast.indexOf(item);
+      let newArr = selectedCast.filter(
+         (e) => selectedCast.indexOf(e) !== index
+      );
+      setSelectedCast(newArr);
+      document.getElementById(item).removeAttribute("disabled");
+      document.getElementById("defaultCast").selectedIndex = 0;
+   };
 
-  return (
-    <div>
-    <div id='filterOptionsContainer'> 
-      <form id="formDefault" onSubmit={handleOnSubmit}>
-        <select
-          id="defaultGenres"
-          className="selectGenres"
-          defaultValue={"DEFAULT"}
-          onChange={(evt) => changeGenres(evt)}
-        >
-          <option value="DEFAULT" disabled className="selectFop">
-            Sort by Genre!
-          </option>
-          {GenresName &&
-            GenresName.length &&
-            GenresName.map((item, index) => {
-              return (
-                <option
-                  id={item}
-                  className="selectFop"
-                  key={index}
-                  value={item}
-                >
-                  {item}
-                </option>
-              );
-            })}
-        </select>
+   return (
+      <div>
+         <div id="filterOptionsContainer">
+            <form id="formDefault" onSubmit={handleOnSubmit}>
+               <select
+                  id="defaultGenres"
+                  className="selectGenres"
+                  defaultValue={"DEFAULT"}
+                  onChange={(evt) => changeGenres(evt)}
+               >
+                  <option value="DEFAULT" disabled className="selectFop">
+                     Sort by Genre!
+                  </option>
+                  {GenresName &&
+                     GenresName.length &&
+                     GenresName.map((item, index) => {
+                        return (
+                           <option
+                              id={item}
+                              className="selectFop"
+                              key={index}
+                              value={item}
+                           >
+                              {item}
+                           </option>
+                        );
+                     })}
+               </select>
 
-        <select
-          id="defaultCast"
-          className="selectCast"
-          defaultValue={"DEFAULT"}
-          onChange={(evt) => changeCast(evt)}
-        >
-          <option disabled value="DEFAULT" className="selectFop">
-            Sort by Cast!
-          </option>
-          {CastName &&
-            CastName.length &&
-            CastName.map((item, index) => {
-              return (
-                <option
-                  id={item}
-                  className="selectFop"
-                  key={index}
-                  value={item}
-                >
-                  {item}
-                </option>
-              );
-            })}
-        </select>
-        <button type="submit">Apply filters</button>
-      </form>
+               <select
+                  id="defaultCast"
+                  className="selectCast"
+                  defaultValue={"DEFAULT"}
+                  onChange={(evt) => changeCast(evt)}
+               >
+                  <option disabled value="DEFAULT" className="selectFop">
+                     Sort by Cast!
+                  </option>
+                  {CastName &&
+                     CastName.length &&
+                     CastName.map((item, index) => {
+                        return (
+                           <option
+                              id={item}
+                              className="selectFop"
+                              key={index}
+                              value={item}
+                           >
+                              {item}
+                           </option>
+                        );
+                     })}
+               </select>
+               <button type="submit">Apply filters</button>
+            </form>
+         </div>
+         <div className="SelectedFilters">
+            {selectedGenres &&
+               selectedGenres.length !== 0 &&
+               selectedGenres.map((item, index) => {
+                  return (
+                     <div key={index}>
+                        <p id="selectedG">{item}</p>
+
+                        <button onClick={() => handleOnClickGenres(item)}>
+                           X
+                        </button>
+                     </div>
+                  );
+               })}
+            {selectedCast &&
+               selectedCast.length !== 0 &&
+               selectedCast.map((item, index) => {
+                  return (
+                     <div key={index}>
+                        <p id="selectedC">{item}</p>
+                        <button onClick={() => handleOnClickCast(item)}>
+                           X
+                        </button>
+                     </div>
+                  );
+               })}
+         </div>
       </div>
-      <div className="SelectedFilters">
-        {selectedGenres &&
-          selectedGenres.length !== 0 &&
-          selectedGenres.map((item, index) => {
-            return (
-              <div key={index}>
-                <p id='selectedG'>{item}</p>
-                
-                <button onClick={() => handleOnClickGenres(item)}>X</button>
-              </div>
-            );
-          })}
-        {selectedCast &&
-          selectedCast.length !== 0 &&
-          selectedCast.map((item, index) => {
-            return (
-              <div key={index}>
-                <p id='selectedC'>{item}</p>
-                <button onClick={() => handleOnClickCast(item)}>X</button>
-              </div>
-            );
-          })}
-      </div>
-    </div>
-  );
+   );
 }
--- client-cine/src/components/home/Home.js
+++ client-cine/src/components/home/Home.js
@@ -7,12 +7,12 @@ import Navbar from "../Navbar/navbar.jsx";
 import Swal from "sweetalert2";
 
 import {
-  AllMovies,
-  GetAllGenres,
-  GetAllCast,
-  FiltrarGenero,
-  FiltrarCast,
-  FiltrarGeneroYCast,
+   AllMovies,
+   GetAllGenres,
+   GetAllCast,
+   FiltrarGenero,
+   FiltrarCast,
+   FiltrarGeneroYCast,
 } from "../../store/actions";
 
 import Movies from "../Movies/Movies.js";
@@ -20,97 +20,101 @@ import Pagination from "../Movies/Pagination";
 import FiltroGeneros from "../filters/filterGenre.js";
 
 const Home = () => {
-  //*dispatch de prueba para las pelis falas que luego sera usado en mostar todas laspelis
+   //*dispatch de prueba para las pelis falas que luego sera usado en mostar todas laspelis
 
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(AllMovies());
-    dispatch(GetAllGenres());
-    dispatch(GetAllCast());
-  }, [dispatch]);
-  const pelisTotales = useSelector((state) => state.PelisAll);
-  const pelisFiltradas = useSelector((state) => state.PelisFiltred);
-  // console.log(pelisFiltradas)
-  const [container, setContainer] = useState([]);
+   const dispatch = useDispatch();
+   useEffect(() => {
+      dispatch(AllMovies());
+      dispatch(GetAllGenres());
+      dispatch(GetAllCast());
+   }, [dispatch]);
+   const pelisTotales = useSelector((state) => state.PelisAll);
+   const pelisFiltradas = useSelector((state) => state.PelisFiltred);
+   // console.log(pelisFiltradas)
+   const [container, setContainer] = useState([]);
 
-  React.useEffect(() => {
-    //luego se añadira filter aqui para decidir si se muestran los resultados filtrados o las pelis
-    if (pelisTotales.length !== 0) {
-      setContainer(pelisTotales);
-    }
-    if (pelisFiltradas.length !== 0) {
-      //Si no hay pelis encontradas popea una alerta y vacía el estado
-      if (
-        pelisFiltradas[0].titulo &&
-        pelisFiltradas[0].titulo === "Movie Not found"
-      ) {
-        Swal.fire("No se encontro peliculas con estos filtros.", "", "error");
-        // alert("No movie found with that sorting");
-        pelisFiltradas.pop();
-      } else {
-        setContainer(pelisFiltradas);
+   React.useEffect(() => {
+      //luego se añadira filter aqui para decidir si se muestran los resultados filtrados o las pelis
+      if (pelisTotales.length !== 0) {
+         setContainer(pelisTotales);
       }
-    }
-  }, [pelisTotales, pelisFiltradas]);
+      if (pelisFiltradas.length !== 0) {
+         //Si no hay pelis encontradas popea una alerta y vacía el estado
+         if (
+            pelisFiltradas[0].titulo &&
+            pelisFiltradas[0].titulo === "Movie Not found"
+         ) {
+            Swal.fire(
+               "No se encontro peliculas con estos filtros.",
+               "",
+               "error"
+            );
+            // alert("No movie found with that sorting");
+            pelisFiltradas.pop();
+         } else {
+            setContainer(pelisFiltradas);
+         }
+      }
+   }, [pelisTotales, pelisFiltradas]);
 
-  //*paginado
-  const [loading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [moviesPerPage] = useState(2);
+   //*paginado
+   const [loading] = useState(false);
+   const [currentPage, setCurrentPage] = useState(1);
+   const [moviesPerPage] = useState(2);
 
-  const indexOfLastPost = currentPage * moviesPerPage;
-  const indexOfFirstPost = indexOfLastPost - moviesPerPage;
-  const currentPost = container.slice(indexOfFirstPost, indexOfLastPost);
+   const indexOfLastPost = currentPage * moviesPerPage;
+   const indexOfFirstPost = indexOfLastPost - moviesPerPage;
+   const currentPost = container.slice(indexOfFirstPost, indexOfLastPost);
 
-  //* Filtros
-  const FiltradoGeneros = (arg) => {
-    if (arg) {
-      dispatch(FiltrarGenero(arg));
-    } else {
-      setContainer(pelisTotales);
-    }
-  };
+   //* Filtros
+   const FiltradoGeneros = (arg) => {
+      if (arg) {
+         dispatch(FiltrarGenero(arg));
+      } else {
+         setContainer(pelisTotales);
+      }
+   };
 
-  const FiltradoCast = (arg) => {
-    dispatch(FiltrarCast(arg));
-  };
+   const FiltradoCast = (arg) => {
+      dispatch(FiltrarCast(arg));
+   };
 
-  const FiltradoGenreAndCast = (arg) => {
-    dispatch(FiltrarGeneroYCast(arg));
-  };
+   const FiltradoGenreAndCast = (arg) => {
+      dispatch(FiltrarGeneroYCast(arg));
+   };
 
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  return (
-    <div>
+   return (
       <div>
-        <React.Fragment>
-          <CssBaseline />
-          <Container maxWidth="ls" sx={{ height: "auto" }}>
-            <Navbar />
-          </Container>
-        </React.Fragment>
-      </div>
+         <div>
+            <React.Fragment>
+               <CssBaseline />
+               <Container maxWidth="ls" sx={{ height: "auto" }}>
+                  <Navbar />
+               </Container>
+            </React.Fragment>
+         </div>
 
-      <Search />
+         <Search />
 
-      <div className="filterContainer">
-        <FiltroGeneros
-          GetAllGenres={GetAllGenres}
-          GetAllCast={GetAllCast}
-          FiltradoGeneros={FiltradoGeneros}
-          FiltradoCast={FiltradoCast}
-          FiltradoGenreAndCast={FiltradoGenreAndCast}
-        />
+         <div className="filterContainer">
+            <FiltroGeneros
+               GetAllGenres={GetAllGenres}
+               GetAllCast={GetAllCast}
+               FiltradoGeneros={FiltradoGeneros}
+               FiltradoCast={FiltradoCast}
+               FiltradoGenreAndCast={FiltradoGenreAndCast}
+            />
+         </div>
+         <Movies moviesInfo={currentPost} loading={loading} />
+         <Pagination
+            className="Home__pagination__li"
+            moviesPerPage={moviesPerPage}
+            totalMovies={container.length}
+            paginate={paginate}
+         />
       </div>
-      <Movies moviesInfo={currentPost} loading={loading} />
-      <Pagination
-        className="Home__pagination__li"
-        moviesPerPage={moviesPerPage}
-        totalMovies={container.length}
-        paginate={paginate}
-      />
-    </div>
-  );
+   );
 };
 export default Home;

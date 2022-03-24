4359af89c73a5bc75cf0a9706e9973f4d66992f2
APinkDev <justapinkdev@gmail.com>
2022-03-24 11:23:39 -0300

    double routes fix

--- client-cine/src/components/App.js
+++ client-cine/src/components/App.js
@@ -20,60 +20,83 @@ import PrivateUpdate from "./PrivateUpdate";
 import ForgotPassword from "./ForgotPassword";
 import UpdateProfile from "./UpdateProfile";
 
-import ReviewToShow from "./ReviewToShow/ReviewToShow.jsx";
 import Admin from "./AdminPanel/Admin.jsx";
 
 export const App = () => {
    return (
-      <Router>
-         <AuthProvider>
-            <Routes>
-               /*Rutas agregadas*/
-               <Route
-                  exact
-                  path="/dash"
-                  element={
-                     <PrivateRoute>
-                        <Dashboard />
-                     </PrivateRoute>
-                  }
-               />
-               <Route path="/signup" element={<Signup />} />
-               <Route path="/login" element={<Login />} />
-               <Route path="/forgot-password" element={<ForgotPassword />} />
-               <Route
-                  path="/update-profile"
-                  element={
-                     <PrivateUpdate>
-                        <UpdateProfile />
-                     </PrivateUpdate>
-                  }
-               />
-               /*Rutas agregadas*/ /*Rutas privadas*/
-               <Route path="/admin" element={<Admin />} />
-               <Route path="/admin/createmovies" element={<CreateMovies />} />
-               <Route path="/admin/createactor" element={<CreateActor />} />
-               <Route path="/admin/creategenero" element={<CreateGenre />} />
-               /*Rutas privadas*/
-               <Route path="/" element={<Home />} />
-               <Route path="/review" element={<Review />} />
-               <Route path="/MovieDetails/:id" element={<DetailsMovies />} />
-               <Route path="/shoppingcart" element={<ShoppingCart />} />
-               <Route path="/about" element={<SobreNosotros />} />
-               <Route path="/portal" element={<Profile />} />
-            </Routes>
-         </AuthProvider>
-
+     <Router>
+ 
+       <AuthProvider>
          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/review/:id" element={<Review />} />
-            <Route path="/reviewtoshow/:id" element={<ReviewToShow />} />
-            <Route path="/MovieDetails/:id" element={<DetailsMovies />} />
-            <Route path="/shoppingcart" element={<ShoppingCart />} />
-            <Route path="/about" element={<SobreNosotros />} />
-
-            <Route path="/portal" element={<Profile />} />
+           /*Rutas agregadas*/
+           <Route
+             exact
+             path="/dash"
+             element={
+               <PrivateRoute component={Dashboard}>
+                 <Dashboard />
+               </PrivateRoute>
+             }
+           />
+           <Route path="/signup" element={<Signup />} />
+           <Route path="/login" element={<Login />} />
+           <Route path="/forgot-password" element={<ForgotPassword />} />
+           <Route
+             path="/update-profile"
+             element={
+               <PrivateUpdate>
+                 <UpdateProfile />
+               </PrivateUpdate>
+             }
+           />
+           /*Rutas agregadas*/ /*Rutas privadas*/
+           <Route
+             path="/admin"
+             element={
+     <PrivateRoute component={Admin}>
+                 
+                 <Admin />
+      </PrivateRoute>
+             }
+           />
+           <Route
+             path="/admin/createmovies"
+             element={
+        <PrivateRoute component={CreateMovies}>
+                 <CreateMovies />
+           </PrivateRoute>
+             }
+           />
+           <Route
+             path="/admin/createactor"
+             element={
+             
+               <PrivateRoute component={CreateActor}>
+                 <CreateActor />
+             </PrivateRoute>
+             }
+           />
+           <Route
+             path="/admin/creategenero"
+             element={
+          
+               <PrivateRoute component={CreateGenre }>
+                 <CreateGenre />
+              </PrivateRoute>
+             }
+           />
+           /*Rutas privadas*/
+           <Route path="/" element={<Home />} />
+           <Route path="/review" element={<Review />} />
+           <Route path="/MovieDetails/:id" element={<DetailsMovies />} />
+           <Route path="/shoppingcart" element={<ShoppingCart />} />
+           <Route path="/about" element={<SobreNosotros />} />
+           <Route path="/portal" element={<Profile />} />
          </Routes>
-      </Router>
+       </AuthProvider>
+ 
+      
+ 
+     </Router>
    );
-};
+ };

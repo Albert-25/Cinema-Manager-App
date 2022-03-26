e54c0045a36ebb19b3d8912355318a2d38bec0da
Alberto-25 <ajst_95@outlook.com>
2022-03-26 16:05:55 -0500

    estilos arreglados

--- client-cine/src/components/ReviewToShow/styled.js
+++ client-cine/src/components/ReviewToShow/styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const DivStar = styled.div`
-    color:${({ value, puntuacion }) => parseInt(value) <= puntuacion ? "#523689 " : "#B2BABB"};
+    color:${({ value, puntuacion }) => parseInt(value) <= puntuacion ? "#523689 " : "#f2f2f25e"};
     padding: 0px 2px;
     font-size: 24px;
     display: inline-block;

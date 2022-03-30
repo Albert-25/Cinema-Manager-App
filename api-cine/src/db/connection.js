a2e19da26ad298283fc548fc67554e686571e799
JSilva723 <silvajonatanivan@gmail.com>
2022-03-30 17:23:04 -0300

    Added ENV=locol for work in local database

--- api-cine/src/db/connection.js
+++ api-cine/src/db/connection.js
@@ -1,36 +1,10 @@
 const { Sequelize } = require("sequelize");
-const { DATABASE_URI } = require("../../config.js");
+const { DATABASE } = require("../../config.js");
 
-// Connection to Heroku database
-const sequelize = new Sequelize(DATABASE_URI, {
+const sequelize = new Sequelize(DATABASE.uri, {
    logging: false,
    native: false,
-   dialectOptions: {
-      require: true,
-      rejectUnauthorized: false,
-   },
-},
-);
-
-// Connection to Heroku database
-// const sequelize = new Sequelize(DATABASE_URI, {
-//    logging: false,
-//    native: false,
-//    dialectOptions: {
-//       ssl: {
-//          require: true,
-//          rejectUnauthorized: false,
-//       },
-//    },
-// });
-
-// sequelize
-//    .authenticate()
-//    .then(() => {
-//       console.log("Connection has been established successfully.");
-//    })
-//    .catch((err) => {
-//       console.error("Unable to connect to the database:", err);
-//    });
+   dialectOptions: DATABASE.opt
+})
 
 module.exports = { sequelize };

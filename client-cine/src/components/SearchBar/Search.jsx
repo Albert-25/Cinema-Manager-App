0bad03fc9ac7662f315cf50af38fd7643c820802
Elian <eliancarlogm@gmail.com>
2022-03-31 02:15:54 -0430

    feat(client/filters) add styles in filters, bilboard and searchbar with bootstrap and css

--- client-cine/src/components/SearchBar/Search.jsx
+++ client-cine/src/components/SearchBar/Search.jsx
@@ -1,39 +1,51 @@
 import React from "react";
-import { Link } from 'react-router-dom';
-import s from './search.module.css'
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { MdClear } from "react-icons/md";
+import s from "./search.module.css";
 
 const searchTitle = (t, a) => {
-  if (t !== '') return a.filter(m => m.titulo.toLowerCase().includes(t.toLowerCase()))
-}
+   if (t !== "")
+      return a.filter((m) => m.titulo.toLowerCase().includes(t.toLowerCase()));
+};
 
-export const Search = ({title,setTitle, items}) => {
+export const Search = ({ title, setTitle, items }) => {
+   const movies = searchTitle(title.trim(), items) || [];
 
-  const movies = searchTitle(title.trim(), items) || []
+   return (
+      <div className={s.container}>
+         <Form.Control
+            placeholder="Ingrese nombre de la pelicula..."
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+         />
+         <Button
+            variant="light"
+            className="d-inline-block ml-3"
+            onClick={() => setTitle("")}
+         >
+            <MdClear />
+         </Button>
 
-  return (
-    <div className={s.container}>
-      <div>
-        <input placeholder="Ingrese nombre de la pelicula..." type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        {title && <span className={s.button} onClick={() => setTitle('')}>🗑️</span>}
-        <ul className={s.list}>
-          {
-            title.trim() !== '' && movies.length === 0
-              ? <p>No se encontro ese titulo</p>
-              : movies.map(movie => {
-                return (
-                  <li key={movie.id}>
-                    <Link to={`MovieDetails/${movie.id}`} className={s.item_list}>
-                      {movie.titulo}
-                    </Link>
-                  </li>
-                )
-              })
-          }
-        </ul>
+         <ul className={s.list}>
+            {title.trim() !== "" && movies.length === 0 ? (
+               <p className={s.item_list}>No se encontro ese titulo</p>
+            ) : (
+               movies.map((movie) => {
+                  return (
+                     <li key={movie.id}>
+                        <Link
+                           to={`MovieDetails/${movie.id}`}
+                           className={s.item_list}
+                        >
+                           {movie.titulo}
+                        </Link>
+                     </li>
+                  );
+               })
+            )}
+         </ul>
       </div>
-    </div>
-  );
-}
+   );
+};
--- client-cine/src/components/bilboard/Bilboard.jsx
+++ client-cine/src/components/bilboard/Bilboard.jsx
@@ -5,30 +5,50 @@ import { Actorsfilter, GenderFilter } from "../filters/Filters";
 import Movies from "../Movies/Movies";
 import Pagination from "../Movies/Pagination";
 import { filterByActors, filterByGenders, filterByTitle } from "./utils";
+import { Row, Col } from "react-bootstrap";
 
 export const Bilboard = () => {
-  const [title, setTitle] = useState("");
-  const [actors, setActors] = useState([]);
-  const [genders, setGenders] = useState([]);
-  const [pageSelected, setPageSelected] = useState(1);
-  const moviesAll = useSelector((state) => state.PelisAll);
-  const moviesByTitle = filterByTitle(moviesAll, title)
-  const moviesByActors = filterByActors(moviesByTitle, actors)
-  const moviesByGenders = filterByGenders(moviesByActors, genders)
-  const moviesPerPage = 4;
-  const end = moviesPerPage * pageSelected;
-  const start = end - moviesPerPage;
-  const filteredMovies = moviesByGenders.slice(start, end)
+   const [title, setTitle] = useState("");
+   const [actors, setActors] = useState([]);
+   const [genders, setGenders] = useState([]);
+   const [pageSelected, setPageSelected] = useState(1);
+   const moviesAll = useSelector((state) => state.PelisAll);
+   const moviesByTitle = filterByTitle(moviesAll, title);
+   const moviesByActors = filterByActors(moviesByTitle, actors);
+   const moviesByGenders = filterByGenders(moviesByActors, genders);
+   const moviesPerPage = 4;
+   const end = moviesPerPage * pageSelected;
+   const start = end - moviesPerPage;
+   const filteredMovies = moviesByGenders.slice(start, end);
 
-  return (
-    <>
-      <Search title={title} setTitle={setTitle} items={moviesAll} />
-      <Actorsfilter setPageSelected={setPageSelected} actors={actors} setActors={setActors} />
-      <GenderFilter setPageSelected={setPageSelected} genders={genders} setGenders={setGenders} />
-      <Movies items={filteredMovies} />
-      {/* <Pagination items={moviesByGenders} setPageSelected={setPageSelected} limit={moviesPerPage} /> */}
-      
-      {filteredMovies.length === 0 && <h1>No hay Resultados</h1>}
-    </>
-  )
-}
+   return (
+      <>
+         <h2
+            style={{ color: "var(--text-light-color)" }}
+            className="mt-5 mb-4 text-center"
+         >
+            Disfruta las mejores peliculas estrenos en
+            <span className="text-primary"> Pantalla Grande</span>
+         </h2>
+         <Row className="mt-5 mb-5 mx-auto">
+            <Col md="8" className="d-flex align-items-center">
+               <Actorsfilter actors={actors} setActors={setActors} />
+               <GenderFilter genders={genders} setGenders={setGenders} />
+            </Col>
+            <Col
+               md="4"
+               className="d-flex align-items-center justify-content-end"
+            >
+               <Search title={title} setTitle={setTitle} items={moviesAll} />
+            </Col>
+         </Row>
+         <Movies items={filteredMovies} />
+         {/* <Pagination
+      items={moviesByGenders}
+      setPageSelected={setPageSelected}
+      limit={moviesPerPage}
+   /> */}
+         {filteredMovies.length === 0 && <h1>No hay Resultados</h1>}
+      </>
+   );
+};

02a76860da7e73b257bf0ef3c244de86e944ceb2
Elian <eliancarlogm@gmail.com>
2022-03-30 21:47:16 -0430

    chores: resolve conflics in the connection to database

--- api-cine/src/db/connection.js
+++ api-cine/src/db/connection.js
@@ -1,44 +1,10 @@
 const { Sequelize } = require("sequelize");
 const { DATABASE } = require("../../config.js");
 
-<<<<<<< HEAD
-// Connection to Heroku database
-/* const sequelize = new Sequelize(DATABASE_URI, {
-   logging: false,
-   native: false,
-   dialectOptions: {
-      require: true,
-      rejectUnauthorized: false,
-   },
-},
-); */
-
-// Connection to Heroku database
-const sequelize = new Sequelize(DATABASE_URI, {
-   logging: false,
-   native: false,
-   dialectOptions: {
-      ssl: {
-         require: true,
-         rejectUnauthorized: false,
-      },
-   },
-});
-
-sequelize
-   .authenticate()
-   .then(() => {
-      console.log("Connection has been established successfully.");
-   })
-   .catch((err) => {
-      console.error("Unable to connect to the database:", err);
-   });
-=======
 const sequelize = new Sequelize(DATABASE.uri, {
    logging: false,
    native: false,
-   dialectOptions: DATABASE.opt
-})
->>>>>>> origin/Developer
+   dialectOptions: DATABASE.opt,
+});
 
 module.exports = { sequelize };

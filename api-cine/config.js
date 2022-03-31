09403332e10618de1af178dcbd1eeef2efb0fd2d
JSilva723 <silvajonatanivan@gmail.com>
2022-03-31 11:15:11 -0300

    Deleted key for jwt

--- api-cine/config.js
+++ api-cine/config.js
@@ -2,11 +2,10 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 1234
 const DATABASE_URI_LOCAL = process.env.DATABASE_URI_LOCAL
 const DATABASE_URI_REMOTO = process.env.DATABASE_URI_REMOTO
-const PRIVATEKEY = process.env.PRIVATEKEY
 const ENV = process.env.ENV
 const DATABASE = {
   uri: ENV === 'local' ? DATABASE_URI_LOCAL : DATABASE_URI_REMOTO,
   opt: ENV === 'local' ? null : { ssl: { require: true, rejectUnauthorized: false } }
 }
 
-module.exports = { PORT, DATABASE, ENV, PRIVATEKEY }
+module.exports = { PORT, DATABASE }

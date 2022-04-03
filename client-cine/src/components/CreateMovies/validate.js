2d2007168951a4b39123c0b3096718e1f1d838c9
Elian <eliancarlogm@gmail.com>
2022-04-03 16:26:02 -0430

    feat(CreateMovies/validate) add some news validations for CreateMovies form

--- client-cine/src/components/CreateMovies/validate.js
+++ client-cine/src/components/CreateMovies/validate.js
@@ -1,111 +1,127 @@
 export function validate(input, stateErrors, name) {
-  let errors = {
-    titulo: stateErrors.titulo,
-    sinopsis: stateErrors.sinopsis,
-    poster: stateErrors.poster,
-    duracion: stateErrors.duracion,
-    clasificacion: stateErrors.clasificacion,
-    director: stateErrors.director,
-    pais: stateErrors.pais,
-    distribuidora: stateErrors.distribuidora,
-    trailer: stateErrors.trailer,
-    genders: stateErrors.genders,
-    actors: stateErrors.actors,
-    error: false,
-  };
-  if (name === "titulo" || name === "submit") {
-    if (!input.titulo && input.titulo !== undefined) {
-      errors.titulo = "Nombre del titulo es requerido";
+   let errors = {
+      titulo: stateErrors.titulo,
+      sinopsis: stateErrors.sinopsis,
+      poster: stateErrors.poster,
+      duracion: stateErrors.duracion,
+      clasificacion: stateErrors.clasificacion,
+      director: stateErrors.director,
+      pais: stateErrors.pais,
+      distribuidora: stateErrors.distribuidora,
+      trailer: stateErrors.trailer,
+      genders: stateErrors.genders,
+      actors: stateErrors.actors,
+      error: false,
+   };
+   if (name === "titulo" || name === "submit") {
+      if (!input.titulo && input.titulo !== undefined) {
+         errors.titulo = "Nombre del titulo es requerido";
+         errors.error = true;
+      } else if (!/^[a-z ,.'-]+$/i.test(input.name)) {
+         errors.name = "Actividad invalida";
+         errors.error = true;
+      } else {
+         errors.titulo = "";
+      }
+   }
+
+   if (name === "sinopsis" || name === "submit") {
+      if (!input.sinopsis && input.sinopsis !== undefined) {
+         errors.sinopsis = "La sinopsis o resumen es requerida";
+         errors.error = true;
+      } else {
+         errors.sinopsis = "";
+      }
+   }
+
+   if (name === "duracion" || name === "submit") {
+      if (!input.duracion && input.duracion !== undefined) {
+         errors.duracion = "Ingrese un numero para la duracion";
+         errors.error = true;
+      } else if (!/^[0-9]+$/.test(input.duracion)) {
+         errors.duracion = "Solo puedes ingresar numeros";
+         errors.error = true;
+      } else {
+         errors.duracion = "";
+      }
+   }
+
+   if (name === "poster" || name === "submit") {
+      if (!input.poster && input.poster !== undefined) {
+         errors.poster = "Ingrese un poster para mostrar";
+         errors.error = true;
+      } else {
+         errors.poster = "";
+      }
+   }
+
+   if (name === "clasificacion" || name === "submit") {
+      if (!input.clasificacion && input.clasificacion !== undefined) {
+         errors.clasificacion = "Defina la clasicacion de la pelicula";
+         errors.error = true;
+      } else {
+         errors.clasificacion = "";
+      }
+   }
+
+   if (name === "pais" || name === "submit") {
+      if (!input.pais && input.pais !== undefined) {
+         errors.pais = "El pais de origen del titulo es requerido";
+         errors.error = true;
+      } else if (!/^[a-z ,.'-]+$/i.test(input.pais)) {
+         errors.pais = "Nombre de pais invalido";
+         errors.error = true;
+      } else {
+         errors.pais = "";
+      }
+   }
+
+   if (name === "director" || name === "submit") {
+      if (!input.director && input.director !== undefined) {
+         errors.director = "El nombre del director es requerido";
+         errors.error = true;
+      } else if (!/^[a-z ,.'-]+$/i.test(input.director)) {
+         errors.pais = "El nombre de director invalido";
+         errors.error = true;
+      } else {
+         errors.director = "";
+      }
+   }
+
+   if (name === "distribuidora" || name === "submit") {
+      if (!input.distribuidora && input.distribuidora !== undefined) {
+         errors.distribuidora = "El distribuidor de la pelicula es requerido";
+         errors.error = true;
+      } else {
+         errors.distribuidora = "";
+      }
+   }
+
+   if (name === "trailer" || name === "submit") {
+      if (!input.trailer && input.trailer !== undefined) {
+         errors.trailer = "El trailer del titulo es requerido";
+         errors.error = true;
+      } else if (
+         /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(
+            input.trailer
+         )
+      ) {
+         errors.trailer = "Ingrese la URL del trailer correctamente";
+         errors.error = true;
+      } else {
+         errors.trailer = "";
+      }
+   }
+
+   if (!input.genders && input.genders !== undefined) {
+      errors.genders = "El genero es requerido";
       errors.error = true;
-    } else if (!/^[a-z ,.'-]+$/i.test(input.name)) {
-      errors.name = "Actividad invalida";
+   }
+
+   if (!input.actors && input.actors !== undefined) {
+      errors.actors = "Los actores son requeridos";
       errors.error = true;
-    } else {
-      errors.titulo = "";
-    }
-  }
+   }
 
-  if (name === "sinopsis" || name === "submit") {
-    if (!input.sinopsis && input.sinopsis !== undefined) {
-      errors.sinopsis = "La sinopsis o resumen es requerida";
-      errors.error = true;
-    } else {
-      errors.sinopsis = "";
-    }
-  }
-
-  if (name === "duracion" || name === "submit") {
-    if (!input.duracion && input.duracion !== undefined) {
-      errors.duracion = "Ingrese un numero para la duracion";
-      errors.error = true;
-    } else {
-      errors.duracion = "";
-    }
-  }
-
-  if (name === "poster" || name === "submit") {
-    if (!input.poster && input.poster !== undefined) {
-      errors.poster = "Ingrese un poster para mostrar";
-      errors.error = true;
-    } else {
-      errors.poster = "";
-    }
-  }
-
-  if (name === "clasificacion" || name === "submit") {
-    if (!input.clasificacion && input.clasificacion !== undefined) {
-      errors.clasificacion = "Defina la clasicacion de la pelicula";
-      errors.error = true;
-    } else {
-      errors.clasificacion = "";
-    }
-  }
-
-  if (name === "pais" || name === "submit") {
-    if (!input.pais && input.pais !== undefined) {
-      errors.pais = "El pais de origen del titulo es requerido";
-      errors.error = true;
-    } else {
-      errors.pais = "";
-    }
-  }
-
-  if (name === "director" || name === "submit") {
-    if (!input.director && input.director !== undefined) {
-      errors.director = "El nombre del director es requerido";
-      errors.error = true;
-    } else {
-      errors.director = "";
-    }
-  }
-
-  if (name === "distribuidora" || name === "submit") {
-    if (!input.distribuidora && input.distribuidora !== undefined) {
-      errors.distribuidora = "El distribuidor de la pelicula es requerido";
-      errors.error = true;
-    } else {
-      errors.distribuidora = "";
-    }
-  }
-
-  if (name === "trailer" || name === "submit") {
-    if (!input.trailer && input.trailer !== undefined) {
-      errors.trailer = "El trailer del titulo es requerido";
-      errors.error = true;
-    } else {
-      errors.trailer = "";
-    }
-  }
-
-  if (!input.genders && input.genders !== undefined) {
-    errors.genders = "El genero es requerido";
-    errors.error = true;
-  }
-
-  if (!input.actors && input.actors !== undefined) {
-    errors.actors = "Los actores son requeridos";
-    errors.error = true;
-  }
-
-  return errors;
+   return errors;
 }

ac83f17b92f1513416b67bd9bd9107b8bc91f90f
APinkDev <justapinkdev@gmail.com>
2022-04-04 16:47:08 -0300

    QR intento y Emails Listos

--- api-cine/src/routes/nodeMailer.js
+++ api-cine/src/routes/nodeMailer.js
@@ -1,24 +1,16 @@
 var nodemailer = require("nodemailer");
 const { Router } = require("express");
 const router = Router();
-let info = {
-    price: 5000,
-    nombre: "Elisa Gonzales",
-    email: "justapinkdev@gmail.com"
-}
-let producto = [{
-    name: "cacerola de acero",
-    quantity: 3,
 
-},
-{
-    name: "macaco de acero",
-    quantity: 1,
 
+const liString = (name, quantity) => {
+    return `<li><span>${name} ${quantity}</span></li>`
 }
-]
 
 const sendEmail = async (req, res) => {
+    let productsString = req.body.products.map((e) => liString(e.name, e.quantity))
+    productsString.join()
+
     var transporter = nodemailer.createTransport({
         service: "Gmail",
         // host: "smtp.ethereal.email",
@@ -32,23 +24,16 @@ const sendEmail = async (req, res) => {
     // ${producto.map((e) => (e.name))} x ${producto.map((e) => (e.quantity))}
     var mailOptions = {
         subject: "Tu compra se ha procesado.",
-        to: info.email,
+        to: req.body.email,
         form: "Thanks~.",
-        html: `<p><b>Saludos</b> Querid@ ${info.nombre} <img src="https://st.depositphotos.com/1967477/3574/v/950/depositphotos_35749475-stock-illustration-cute-monkey-giving-thumb-up.jpg"/></p>
+        html: `<p><b>Saludos</b> Querid@ ${req.body.name} <img src="https://st.depositphotos.com/1967477/3574/v/950/depositphotos_35749475-stock-illustration-cute-monkey-giving-thumb-up.jpg"/></p>
         <p>su compra en nuestro sitio se ha completado recientemente, aqui le enviaremos<br/>
         la informacion de lo que compro y un codigo QR para llevarlo y mostrarlo en el cine~<br/>
         Usted compro:<br/></p>
         Productos: <br/>
-        <p>
-        ${producto.map((a) =>( 
-            <li key={a}>
-                <span>{a}</span>
-            </li>
-        ))}
-        </p>
-        
-        Precio total:${info.price} pesos brasileños <br/>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/d/d7/Commons_QR_code.png" alt="qr"/>`,
+        ${productsString}
+        Precio total:${req.body.price} pesos brasileños <br/>
+        <img src=${req.body.QR} alt="qr"/>`,
     }
 
     transporter.sendMail(mailOptions, (error, info) => {
